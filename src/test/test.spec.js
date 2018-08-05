21c908b1af3c9b829c4d7c0ce7e375ad55059a2a
David Machado <david.machado@mercadolibre.com>
2018-08-05 19:06:19 -0300

    add third and fourth test in which we request a class for at least 4/6 square meters and get Lab A/B that meet the requirements

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -3,13 +3,14 @@ import { expect } from 'chai';
 import Classroom from '../main/classrooms/Classroom';
 import NoClassroom from '../main/classrooms/NoClassroom';
 
+import Persons from '../main/classrooms/specs/Persons';
+import SquareMeters from '../main/classrooms/specs/SquareMeters';
+
 import CapacityOfAtLeast from '../main/conditions/CapacityOfAtLeast';
 
-import RequestForClassroom from '../main/RequestForClassroom';
-
 import Label from '../main/Label';
-import Persons from '../main/Persons';
 import ClassroomsPool from '../main/ClassroomsPool';
+import RequestForClassroom from '../main/RequestForClassroom';
 
 describe('Search for classroom', () => {
   
@@ -18,8 +19,16 @@ describe('Search for classroom', () => {
   var pool;
 
   beforeEach(function() {
-    labA = new Classroom(new Label('Lab A'), new Persons(5));
-    labB = new Classroom(new Label('Lab B'), new Persons(15));
+    labA = new Classroom(
+      new Label('Lab A'), 
+      new Persons(5),
+      new SquareMeters(5)
+    );
+    labB = new Classroom(
+      new Label('Lab B'), 
+      new Persons(15),
+      new SquareMeters(15)
+    );
 
     pool = new ClassroomsPool(labA, labB);
   });
@@ -41,6 +50,22 @@ describe('Search for classroom', () => {
   it('for at least 5 persons should return Lab B', () => {
     let classroom = pool.searchClassroomFor(
       new RequestForClassroom(new CapacityOfAtLeast(new Persons(10)))
+    );
+
+    expect(classroom.toJSON()).to.be.equal(labB.toJSON());
+  });
+
+  it('with at least 4 square meters should return lab A', () => {
+    let classroom = pool.searchClassroomFor(
+      new RequestForClassroom(new CapacityOfAtLeast(new SquareMeters(4)))
+    );
+
+    expect(classroom.toJSON()).to.be.equal(labA.toJSON());
+  });
+
+  it('with at least 6 square meters should return lab B', () => {
+    let classroom = pool.searchClassroomFor(
+      new RequestForClassroom(new CapacityOfAtLeast(new SquareMeters(6)))
     );
 
     expect(classroom.toJSON()).to.be.equal(labB.toJSON());
